2836455ed569055725c096e83540c7743ef5afc5
jish0desarkar <desarkarjishnu@gmail.com>
2025-12-06 01:06:28 +0530

    IMPROVEMENT: Batch process zAdd during sync

--- apps/scheduler/src/utils/sync-redis.ts
+++ apps/scheduler/src/utils/sync-redis.ts
@@ -3,26 +3,33 @@ import { query } from "../db/client.ts";
 import { activeEnpointQueryType } from "../db/query/get-active-endpoints.ts";
 
 export async function syncRedis() {
+  const BATCH_SIZE = 10;
   const lastSyncedAt = await RedisClient.get("lastSynedAt");
   let endpointsToBeSynced: Array<activeEnpointQueryType>;
   if (lastSyncedAt == null) {
     endpointsToBeSynced = (
       await query(
-        "SELECT endpoint, is_active, check_interval, expected_status_code FROM configs"
+        "SELECT endpoint, is_active, EXTRACT(epoch from (NOW() + check_interval))::bigint AS next_run_at, expected_status_code FROM configs"
       )
     ).rows;
   } else {
     endpointsToBeSynced = (
       await query(
-        "SELECT endpoint, is_active, check_interval, expected_status_code FROM configs WHERE updated_at > $1",
+        "SELECT endpoint, is_active, EXTRACT(epoch from (NOW() + check_interval))::bigint AS next_run_at, expected_status_code FROM configs WHERE updated_at > $1",
         [lastSyncedAt]
       )
     ).rows;
   }
-  endpointsToBeSynced.forEach((element) => {
-    RedisClient.zAdd("next_run_at", {
-      score: element.next_run_at,
-      value: element.endpoint,
-    });
-  });
+  for (let i = 0; i < endpointsToBeSynced.length; i += BATCH_SIZE) {
+    const endpointSlice = endpointsToBeSynced.slice(i, i + BATCH_SIZE);
+    await RedisClient.zAdd(
+      "next_run_at",
+      endpointSlice.map((el) => {
+        return {
+          score: el.next_run_at,
+          value: el.endpoint,
+        };
+      })
+    );
+  }
 }
